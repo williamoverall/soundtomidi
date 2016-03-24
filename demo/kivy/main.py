4b4fbb454fdb8e03868532ea4f5cd5832c815824
williamoverall <william.overall@gmail.com>
2016-03-24 07:48:10 -0400

    Getting started with kivy demo

--- demo/kivy/main.py
+++ demo/kivy/main.py
@@ -1,39 +1,27 @@
 from kivy.app import App
+from kivy.factory import Factory
+from kivy.lang import Builder, Parser
+from kivy.properties import ObjectProperty
 from kivy.clock import Clock
-from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.boxlayout import BoxLayout
 
-class MainScreen(Screen):
-    def start(self):
-        pass
+class RootLayout(BoxLayout):
+    screen_manager = ObjectProperty()
 
-    def update(self,dt):
-        print("Updating")
+    def __init__(self, **kwargs):
+        super(RootLayout, self).__init__(**kwargs)
+        self.show_kv(None, 'Monitor')
 
-class SettingsScreen(Screen):
-    pass
-
-class BeatsScreenManager(ScreenManager):
-    def __init__(self):
-        ScreenManager.__init__(self)
-        self.add_widget(MainScreen(name='main'))
-        self.add_widget(SettingsScreen(name='settings'))
-
-    def start(self):
-        pass
-
-    def update(self,dt):
+    def show_kv(self, instance, value):
+        self.screen_manager.current = value
+        child = self.screen_manager.current_screen.children[0]
+        with open(child.kv_file, 'rb') as file:
+            self.language_box.text = file.read().decode('utf8')
 
 
 class BeatsApp(App):
     def build(self):
-        clock_update_interval = 1.0/60.0
-        screen_manager = BeatsScreenManager()
-        screen_manager.start()
-
-        Clock.max_iteration = 40
-        Clock.schedule_interval(screen_manager.update, clock_update_interval)
-        return screen_manager
+        return RootLayout()
 
 if __name__ == '__main__':
     BeatsApp().run()
