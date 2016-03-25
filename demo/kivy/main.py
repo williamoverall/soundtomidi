ef0a92870626e55ab095633b67aeea30ecdf5522
Bill <william.overall@gmail.com>
2016-03-25 15:30:27 -0400

    More work on kivy demo

--- demo/kivy/main.py
+++ demo/kivy/main.py
@@ -1,23 +1,17 @@
 from kivy.app import App
+from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.factory import Factory
 from kivy.lang import Builder, Parser
 from kivy.properties import ObjectProperty
 from kivy.clock import Clock
 from kivy.uix.boxlayout import BoxLayout
+from kivy.config import Config
+
+Config.set('graphics', 'width', '800')
+Config.set('graphics', 'height', '480')
 
 class RootLayout(BoxLayout):
     screen_manager = ObjectProperty()
-
-    def __init__(self, **kwargs):
-        super(RootLayout, self).__init__(**kwargs)
-        self.show_kv(None, 'Monitor')
-
-    def show_kv(self, instance, value):
-        self.screen_manager.current = value
-        child = self.screen_manager.current_screen.children[0]
-        with open(child.kv_file, 'rb') as file:
-            self.language_box.text = file.read().decode('utf8')
-
 
 class BeatsApp(App):
     def build(self):
